a28e94a95eedea97fb7e4d844c210e4c09943535
yhk313 <kyeong4099@naver.com>
2023-11-14 02:21:02 +0900

    Init: 패키지 구조 설정

---
+++ src/test/java/wanted/budgetmanagement/app/BudgetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package wanted.budgetmanagement.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
